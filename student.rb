20490694afc14a05f4dcb1aa3d6b71d709387d6d
Aleksandra Ujvari <ujvari65@gmail.com>
2022-09-26 14:34:22 +0200

    Add classes person, student and teacher

---
+++ student.rb
@@ -0,0 +1,20 @@
+require_relative './person'
+
+class Student < Person
+  # holding the relationship
+  attr_reader :classroom
+
+  def initialize(_classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+end
