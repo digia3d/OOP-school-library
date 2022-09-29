e6b33409d80f47258e6adfa35e683084279ed63b
Aleksandra Ujvari <ujvari65@gmail.com>
2022-09-29 20:27:04 +0200

    Add class app and method for start and menu

---
+++ app.rb
@@ -0,0 +1,34 @@
+require './person'
+require './student'
+require './teacher'
+require './classroom'
+require './book'
+require './rental'
+
+class App
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+  end
+
+  def start_app
+    print 'Welcome To School Library App'
+    sleep 0.75
+    menu
+  end
+
+  def menu
+    puts
+    puts 'Please choose an option by entering a number'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    option = gets.chomp
+
+    get_option option
+  end
