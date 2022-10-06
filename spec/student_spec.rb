7e74270a6df7f677cc760fd7eae3108c1b882157
Kinyua <alfkinyua@gmail.com>
2022-10-06 19:06:31 +0300

    Append classroom test to new student

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -6,6 +6,12 @@ describe Student do
         new_student = Student.new(nil, 21, 'Alfred', true)
         expect(new_student.name).to eq 'Alfred'
       end
+      it 'Add a student to a specific class' do
+        new_student = Student.new(nil, 21, 'Alfred', true)
+        new_classromm = Classroom.new('Computer science')
+        new_student.classroom =new_classromm
+        expect(new_student.classroom).to eq new_classromm
+      end
     end
     
 end
